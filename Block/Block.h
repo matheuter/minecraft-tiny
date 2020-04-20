01ea7278c1aa118007359d67eee33f04f54b5eba
Euter <210880040@qq.com>
2020-04-20 21:18:38 +0800

    add files

--- Block/Block.h
+++ Block/Block.h
@@ -1,15 +1,18 @@
-//
-// Created by bainsky on 2020/4/20.
-//
-
 #ifndef MINECRAFT_BLOCK_H
 #define MINECRAFT_BLOCK_H
 
+#include "../Texture/Texture.h"
 #include "../GL/gl_imp.h"
 class Block
 {
 private:
 	glm::vec3 m_position;
+	Texture* m_pTexture;
+public:
+	Block() = default;
+	~Block();
+
+	virtual void drawBlock();
 };
 
 
